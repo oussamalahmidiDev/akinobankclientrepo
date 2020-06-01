27c4479e01e911a538a603f0360fabce8f3c087f
OussamaDev <oussama.lahmidi@icloud.com>
2020-06-01 16:03:35 +0100

    Added session auto logout after 1 minute of inactivity.

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,30 +1,63 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Select } from "@ngxs/store";
-import { Observable } from "rxjs";
+import { Observable, timer, of, Subscription } from "rxjs";
 import { User } from "../../models/user";
 import { ProfileState } from "../../states/profile.state";
-import {Router} from "@angular/router";
-import {TokenService} from "../../services/token.service";
+import { Router } from "@angular/router";
+import { TokenService } from "../../services/token.service";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   isLoaded = true;
 
   @Select(ProfileState.selectProfile)
   currentUser: Observable<User>;
 
+  timer: Observable<number>;
+
+  MAX_TIME = 60;
+
+  subscription: Subscription;
+
   ngOnInit() {
-    // this.currentUser = this.activatedRoute.snapshot.data.profile;
+    window.onblur = (e) => {
+      console.log("Tab/window changed", e);
+      this.timer = timer(0, 1000);
+      this.subscription = new Subscription();
+      this.subscription.add(
+        this.timer.subscribe((time) => {
+          console.log("Timer", this.MAX_TIME - time);
+          if (this.MAX_TIME - time === 0) {
+            this.logout();
+            alert("La session a été deconnecté à cause de l'inactivité.");
+          }
+        })
+      );
+    };
+
+    window.onfocus = () => {
+      console.log("Tab/window focused");
+      this.timer = undefined;
+      if (this.subscription) this.subscription.unsubscribe();
+    };
   }
 
   logout(): void {
     this.tokenService.unsetToken();
     this.router.navigate(["/"]);
     // this.userService.logout();
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    if (this.subscription) this.subscription.unsubscribe();
+    window.onblur = window.onfocus = undefined;
   }
 
   constructor(private router: Router, private tokenService: TokenService) {}
